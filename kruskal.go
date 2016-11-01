263181b5128df135e623a85ff1bbd15a4fbf232a
Luke LaFountaine <luke.lafountaine537@myci.csuci.edu>
2016-11-01 13:28:46 -0700

    added functions to check if an edge creates a cycle and to connect components

--- kruskal.go
+++ kruskal.go
@@ -10,9 +10,10 @@ import (
 )
 
 type Edge struct {
-	Node1, Node2, Weight int64
+	Node1, Node2, Weight int
 }
 
+// type to sort by edge weight
 type ByWeight []Edge
 
 func (a ByWeight) Len() int {
@@ -25,11 +26,42 @@ func (a ByWeight) Less(i, j int) bool {
 	return a[i].Weight < a[j].Weight
 }
 
-func kruskall(edges []Edge, dim int) []Edge {
-	sort.Sort(ByWeight(edges))
-	fmt.Println(edges)
+func kruskal(edges []Edge, dim int) []Edge {
 
-	return nil
+	sort.Sort(ByWeight(edges))
+
+	// create the slice of connected components
+	components := make([]int, dim*dim)
+	for i := 0; i < dim*dim; i++ {
+		components[i] = i
+	}
+
+	mcst := make([]Edge, 0)
+
+	for _, edge := range edges {
+		node1, node2 := edge.Node1, edge.Node2
+		if !createsCycle(components, node1, node2) {
+			connectComponents(components, node1, node2)
+			mcst = append(mcst, edge)
+		}
+	}
+
+	return mcst
+}
+
+func createsCycle(components []int, node1, node2 int) bool {
+	return components[node1-1] == components[node2-1]
+}
+
+func connectComponents(components []int, node1, node2 int) {
+	new := components[node1-1]
+	old := components[node2-1]
+
+	for i := range components {
+		if components[i] == old {
+			components[i] = new
+		}
+	}
 }
 
 func main() {
@@ -61,9 +93,9 @@ func main() {
 
 		edge := strings.Split(x, ",")
 
-		start, err:= strconv.ParseInt(edge[0], 10, 64)
-		end, err := strconv.ParseInt(edge[1], 10, 64)
-		weight, err := strconv.ParseInt(edge[2], 10, 64)
+		start, err:= strconv.Atoi(edge[0])
+		end, err := strconv.Atoi(edge[1])
+		weight, err := strconv.Atoi(edge[2])
 
 		if err != nil {
 			fmt.Println(err)
@@ -73,8 +105,6 @@ func main() {
 		edges = append(edges, Edge{start, end, weight})
 	}
 
-	fmt.Println(dim, edges)
-	mcst := kruskall(edges, dim)
-	fmt.Println(mcst, edges)
-
+	mcst := kruskal(edges, dim)
+	fmt.Println(mcst)
 }
